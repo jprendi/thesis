28b8d27882b7a1539626e2763687caa189edad4a
jprendi <jprendi@student.ethz.ch>
2024-03-01 15:43:45 +0100

    explore_dataset created and bsm_keys function added to dataset.py

--- scripts/dataset.py
+++ scripts/dataset.py
@@ -12,8 +12,8 @@ def load_dataset(dataset, key):
     return datss.reshape(len(datss), 99)
 
 
-def create_xtrain_xtest(random_seed=1):
-    datt = load_dataset('NuGun_preprocessed.h5', 'full_data_cyl')
+def create_xtrain_xtest(random_seed=1, background_data='NuGun_preprocessed.h5', bkg_key='full_data_cyl'):
+    datt = load_dataset(background_data, bkg_key)
     x_train ,x_test = sklearn.model_selection.train_test_split(datt, random_state=random_seed)
     return x_train, x_test
 
@@ -43,3 +43,21 @@ def inject_signal(bkg, sig, size, percentage, random_seed=1):
     new_dataset = np.concatenate((newdat1, newdat2))
 
     return new_dataset, labels, (newdat1, newdat2), (size1, size2)
+
+
+
+def get_all_bsm_keys(dataset='BSM_preprocessed.h5'):
+    listing = []
+    with h5py.File(dataset, 'r') as file:
+        for i in file.keys():
+            listing.append([i, np.shape(file[i])])
+    return listing
+
+
+def bsm_keys(dataset='BSM_preprocessed.h5'):
+    lists = get_all_bsm_keys(dataset)
+    potential=[]
+    for i in range(len(lists)):
+        if np.shape(lists[i][1]) == (3,):
+            potential.append(lists[i])
+    return potential
