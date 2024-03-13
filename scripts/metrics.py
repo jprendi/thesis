5d1ba9c7a29eeb8b090ab49f5276bf167ab567a3
jprendi <jprendi@student.ethz.ch>
2024-03-13 22:27:43 +0100

    kfold scan w.r.t. different scoring metrics of the GluGluHToTauTau signal !!

--- scripts/metrics.py
+++ scripts/metrics.py
@@ -22,7 +22,6 @@ def plot_roc(FPR, TPR, AUC):
     plt.legend(loc='lower right',fontsize=15)
     plt.tight_layout()
 
-
 def get_roc_auc(score_0, score_1):
     labels = np.concatenate((np.ones(len(score_1)), np.zeros(len(score_0))))
     all = np.concatenate((score_1, score_0))
@@ -30,10 +29,8 @@ def get_roc_auc(score_0, score_1):
     AUC = auc(FPR, TPR)
     return FPR, TPR, AUC
 
-
 def train_and_save_model(kwargs, x_train):
   # kwargs argument is a dictionary
-
   model_string = "trained_models/model__" + "__".join([f"{key}_{value}" for key, value in kwargs.items()]) # this line is thanks to chatgpt
   if os.path.isfile(model_string)==True:
     print("This model already exists!")
@@ -41,8 +38,7 @@ def train_and_save_model(kwargs, x_train):
   else:
     model = IsolationForest(**kwargs).fit(x_train)
     pickle.dump(model, open(model_string, 'wb'))
-    return model_string
-  
+    return model_string  
 
 def predict_value(model, x_test, signal, output1="score"):
     loaded_model = pickle.load(open(model, 'rb'))
@@ -50,7 +46,6 @@ def predict_value(model, x_test, signal, output1="score"):
     score_signal = loaded_model.predict(signal, output=output1)
     fpr, tpr, auc = get_roc_auc(score_x_test, score_signal)
     return fpr, tpr, auc
-
 
 # for 1 thing (for now?:))
 
@@ -68,19 +63,19 @@ def get_seed_uncertainty(kwargs, x_test, signal):
 
     interp_tpr={}
     base = np.exp(np.linspace(math.log(0.00000005), 0., 1000000)) # bc we are interested in the loglog plot later! :)
-    seeds = [0,1,2,3,4]#,39, 23123]
+    seeds = [0,1,2,3,4]
     for i in seeds:
         interp_tpr[i] = np.interp(base, fprs[i], tprs[i])
 
     mean_curve = np.mean(list(interp_tpr.values()), axis=0)
     error_curve = np.std(list(interp_tpr.values()), axis=0)
+    auc_mean = np.mean(list(aucs.values()))
+    auc_unc = np.std(list(aucs.values()))
 
-    return mean_curve, error_curve, base
+    return mean_curve, error_curve, base, auc_mean, auc_unc
 
-
-
-def plot_auroc_unc_(mean_curve, error_curve, base):    
-    plt.plot(base,mean_curve, linewidth=1.5)
+def plot_auroc_unc_(mean_curve, error_curve, base, auc_mean, auc_unc): 
+    plt.plot(base,mean_curve, linewidth=0.5,  label='BB (AUC = %.1f%% $\pm$ %.1f%%)' % (auc_mean * 100, auc_unc * 100))
     plt.semilogx()
     plt.semilogy()
     plt.fill_between(base,
@@ -88,4 +83,4 @@ def plot_auroc_unc_(mean_curve, error_curve, base):
                 mean_curve + error_curve,
                 alpha=0.5,
             label = 'depth')
-    plt.legend()
+    plt.legend(loc='lower right',fontsize=15)
