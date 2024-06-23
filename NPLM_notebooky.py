14262148a399c6c9076577fb296b7085f9c52bc9
jprendi <jessica.prendi@outlook.de>
2024-06-24 01:03:29 +0200

    for doing the NPLM scans ive been wanting to do for a while with the reweighting!!

---
+++ NPLM_notebooky.py
@@ -0,0 +1,225 @@
+'''
+all from this with *very* small adaptions:
+
+https://github.com/GaiaGrosso/NPLM_package/blob/v0.0.6/example_1D/1D_tutorial.ipynb
+'''
+
+
+import sys, os, time, datetime, h5py
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.font_manager as font_manager
+from scipy.stats import norm, expon, chi2, uniform, chisquare
+
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.constraints import Constraint
+from tensorflow.keras import metrics, losses, optimizers
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Dense, Input, Layer
+from tensorflow import Variable
+from tensorflow import linalg as la
+
+from NPLM.NNutils import *
+from NPLM.PLOTutils import *
+from NPLM.ANALYSISutils import *
+
+from scripts.physics import invariant_mass
+
+
+def run_NPLM(ZBT, NGT,  NG_weights, seed=1, 
+                total_epochs_tau = 20000,
+                plot=False, plot_reconstructions=False,
+                plot_loss=False):
+    np.random.seed(seed)
+    total_epochs_tau = total_epochs_tau
+    patience_tau = 1000
+    inputsize          = 1
+    latentsize         = 4
+    n_layers           = 1
+    BSMweight_clipping = 9
+    layers             = [inputsize]
+    for _ in range(n_layers):
+        layers.append(latentsize)
+    layers.append(1)
+    # print(layers)
+    hidden_layers      = layers[1:-1]
+    BSMarchitecture    = layers
+    BSMdf              = compute_df(input_size=BSMarchitecture[0], hidden_layers=BSMarchitecture[1:-1])
+
+    featureReff, ind = invariant_mass(NGT, type='jet', return_indices = True)
+    featureData = ZBT
+    # np.array(invariant_mass(ZBT, type='jet')).reshape((-1,1))
+
+    featureRef = np.array(featureReff).reshape((-1,1))
+    feature    = np.concatenate((featureData, featureRef), axis=0)
+
+    # # target                                                                                                                                                         
+    targetData  = np.ones_like(featureData)
+    targetRef   = np.zeros_like(featureRef)
+    weightsData = np.ones_like(featureData)#*ratiooooo
+
+    weightsRef_1 = np.array(NG_weights)[ind].reshape((-1,1))
+
+    counts_D, bin_edges_D= np.histogram(featureData,  weights=weightsData, bins=100, range=(0,4000))
+    counts_R, bin_edges_R = np.histogram(featureRef,  weights=weightsRef_1, bins=100, range=(0,4000))
+
+    integral_D= np.sum(counts_D * np.diff(bin_edges_D))
+    integral_R= np.sum(counts_R * np.diff(bin_edges_R))
+
+    ratiooo = integral_D/integral_R
+
+
+    weightsRef = weightsRef_1*ratiooo#
+
+    target_0    = np.concatenate((targetData, targetRef), axis=0)
+    weights     = np.concatenate((weightsData, weightsRef), axis=0)
+    target      = np.concatenate((target_0, weights), axis=1)
+
+    batch_size  = feature.shape[0]
+    inputsize   = feature.shape[1]
+
+    Scale   = 0
+    Norm    = 0
+    sigma_s = 0.15
+    sigma_n = 0.15
+    NU0_S     = np.random.normal(loc=Scale, scale=sigma_s, size=1)[0]
+    NU0_N     = np.random.normal(loc=Norm, scale=sigma_n, size=1)[0]
+    NUR_S     = np.array([0. ])
+    NUR_N     = 0
+    NU_S      = np.array([0. ])
+    NU_N      = 0
+    SIGMA_S   = np.array([sigma_s])
+    SIGMA_N   = sigma_n 
+                                                                                                                                                        
+    input_shape = (None, inputsize)                
+
+    weights_file  = './LINEAR_Parametric_EXPO1D_batches_ref40000_bkg40000_sigmaS0.1_-1.0_-0.5_0.5_1.0_patience300_epochs30000_layers1_4_1_actrelu_model_weights9300.h5'
+
+    sigma = weights_file.split('sigmaS', 1)[1]                                                                                                                 
+    sigma = float(sigma.split('_', 1)[0])                                                                                                                        
+    scale_list=weights_file.split('sigma', 1)[1]                                                                                   
+    scale_list=scale_list.split('_patience', 1)[0]                                                                                                               
+    scale_list=np.array([float(s) for s in scale_list.split('_')[1:]])*sigma  
+    print(scale_list)
+    shape_std = np.std(scale_list)
+    activation= weights_file.split('act', 1)[1]
+    activation=activation.split('_', 1)[0]
+    wclip=None
+    if 'wclip' in weights_file:
+        wclip= weights_file.split('wclip', 1)[1]
+        wclip = float(wclip.split('/', 1)[0])
+
+    layers=weights_file.split('layers', 1)[1]
+    layers= layers.split('_act', 1)[0]
+    architecture = [int(l) for l in layers.split('_')]
+
+    poly_degree = 0
+    if 'LINEAR' in weights_file:
+        poly_degree = 1
+    elif 'QUADRATIC' in weights_file:
+        poly_degree = 2
+    elif 'CUBIC' in weights_file:
+        poly_degree = 3
+    else:
+        print('Unrecognized number of degree for polynomial parametric net in file: \n%s'%(weights_file))
+        poly_degree = None
+        
+    scale_parNN = {
+        'poly_degree'   : poly_degree,
+        'architectures' : [architecture for i in range(poly_degree)],
+        'wclips'        : [wclip for i in range(poly_degree)],
+        'activation'    : activation,
+        'shape_std'     : shape_std,
+        'weights_file'  : weights_file
+        }
+
+    parNN_list = { 
+        'scale': scale_parNN,
+        }
+
+    tau = imperfect_model(input_shape=input_shape,
+                NU_S=NUR_S, NUR_S=NUR_S, NU0_S=NU0_S, SIGMA_S=SIGMA_S, 
+                NU_N=NUR_N, NUR_N=NUR_N, NU0_N=NU0_N, SIGMA_N=SIGMA_N,
+                correction='SHAPE', shape_dictionary_list=[parNN_list['scale']],
+                BSMarchitecture=BSMarchitecture, BSMweight_clipping=BSMweight_clipping, train_f=True, train_nu=True)
+    print(tau.summary())
+
+    tau.compile(loss=imperfect_loss,  optimizer='adam')
+
+    hist_tau = tau.fit(feature, target, batch_size=batch_size, epochs=total_epochs_tau, verbose=False)
+
+    loss_tau  = np.array(hist_tau.history['loss'])
+    # test statistic                                                                                                                                                                                                                                                                                                               
+    final_loss = loss_tau[-1]
+    tau_OBS    = -2*final_loss
+    print('tau_OBS: %f'%(tau_OBS))
+
+    if plot_loss == True:
+        fig = plt.figure(figsize=(9,6))                                                                                                                                            
+        fig.patch.set_facecolor('white') 
+        plt.plot(loss_tau, label='tau')
+        font=font_manager.FontProperties(family='serif', size=18)
+        plt.legend(prop=font)
+        plt.ylabel('Loss', fontsize=18, fontname='serif')
+        plt.xlabel('Epochs', fontsize=18, fontname='serif')
+        plt.xticks(fontsize=16, fontname='serif')
+        plt.yticks(fontsize=16, fontname='serif')
+        # plt.ylim(-3600,-2500)
+        plt.grid()
+        plt.show()
+        plt.close()
+
+
+    if plot==True:
+        bins_code = {                                                                                                                                                                                                                                                                         
+            'mass': np.arange(0, 4000, 50
+                            
+                            )                                                                                                                             
+            }  
+        ymax_code = {                                                                                                                                                                                                                                                                       
+            'mass': 5                                                                                                                          
+            }  
+        xlabel_code = {                                                                                                                                                                                                                                                                        
+            'mass': r'$m_{jj}$',                                                                                                                            
+            }  
+        feature_labels = list(bins_code.keys())
+
+        REF    = feature[target[:, 0]==0]
+        DATA   = feature[target[:, 0]==1]
+        weight = target[:, 1]
+        weight_REF       = weight[target[:, 0]==0]
+        weight_DATA      = weight[target[:, 0]==1]
+        plot_training_data(data=DATA, weight_data=weight_DATA, ref=REF, weight_ref=weight_REF, 
+                        feature_labels=feature_labels, bins_code=bins_code, xlabel_code=xlabel_code, ymax_code=ymax_code)
+                        #    save=True, save_path='thesis', file_name='no_reweight')
+
+    if plot_reconstructions == True:
+
+        REF    = feature[target[:, 0]==0]
+        DATA   = feature[target[:, 0]==1]
+        weight = target[:, 1]
+
+        weight_REF       = weight[target[:, 0]==0]
+        weight_DATA      = weight[target[:, 0]==1]
+        output_tau_ref   = tau.predict(REF)
+
+
+        bins_code = {                                                                                                                                                                                                                                                                         
+            'mass': np.arange(0, 4000,40)                                                                                                                             
+            }  
+        ymax_code = {                                                                                                                                                                                                                                                                       
+            'mass': 5                                                                                                                        
+            }  
+        xlabel_code = {                                                                                                                                                                                                                                                                        
+            'mass': r'$m_{jj}$',                                                                                                                            
+            }  
+        feature_labels = list(bins_code.keys())
+
+        plot_reconstruction(df=BSMdf, data=DATA, weight_data=weight_DATA, ref=REF, weight_ref=weight_REF, 
+                            tau_OBS=tau_OBS, output_tau_ref=output_tau_ref,  
+                            feature_labels=feature_labels, bins_code=bins_code, xlabel_code=xlabel_code, ymax_code=ymax_code)#,
+                            # delta_OBS=delta_OBS, output_delta_ref=output_delta_ref, 
+                            # save=True, save_path='thesis', file_name='no_reweight_reco')
+
+    return tau_OBS
---
+++ get_2Dhistogrids.py
@@ -0,0 +1,188 @@
+from model_data_sculpting import sculpt_study
+# from scripts import dataset
+from sklearn.model_selection import train_test_split
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.colors as mcolors
+
+
+# class do_NPLM_test(): 
+#     def __init__(self, sigkey, 
+#                  model_dir='trained_models/isotree/kfold_models/', 
+#                  model_name='model__ntrees_100__scoring_metric_depth__fold_1_of_5',
+#                  dimass_type='jet', rand=1):
+
+#         sculpts = sculpt_study(model_dir=model_dir, model_name=model_name, signal=sigkey, dimass_type=dimass_type)
+
+#         ZB_Bottom = sculpt_study.select_anomalous_datapoints(sculpts.ZeroBias_anomaly_score, sculpts.ZeroBias_data, area='bottom', percentage=0.95)
+#         NG_Bottom = sculpt_study.select_anomalous_datapoints(sculpts.NuGun_anomaly_score, sculpts.NuGun_data, area='bottom', percentage=0.95)
+
+#         top_ZB = sculpt_study.select_anomalous_datapoints(anomaly_score=sculpts.ZeroBias_anomaly_score, dataset=sculpts.ZeroBias_data, area='top', percentage=0.05)
+#         top_NG = sculpt_study.select_anomalous_datapoints(anomaly_score=sculpts.NuGun_anomaly_score, dataset=sculpts.NuGun_data, area='top', percentage=0.05)
+
+
+# class NPLM_ish():
+#     ### here the thing that prepares NPLM
+
+
+    # here the thing that executes it all....
+
+class create_reweight_maps():
+    def __init__(self, sigkey, 
+                 model_dir='trained_models/isotree/kfold_models/', 
+                 model_name='model__ntrees_100__scoring_metric_depth__fold_1_of_5',
+                 dimass_type='jet'):
+
+        sculpts = sculpt_study(model_dir=model_dir, model_name=model_name, signal=sigkey, dimass_type=dimass_type)
+
+        ZB_Bottom = sculpt_study.select_anomalous_datapoints(sculpts.ZeroBias_anomaly_score, sculpts.ZeroBias_data, area='bottom', percentage=0.95)
+        NG_Bottom = sculpt_study.select_anomalous_datapoints(sculpts.NuGun_anomaly_score, sculpts.NuGun_data, area='bottom', percentage=0.95)
+
+        self.ratio1 = 0
+        self.ratio2 = 0
+
+        self.get_grids(ZB_Bottom, NG_Bottom)
+     
+        top_ZB_pre = sculpt_study.select_anomalous_datapoints(anomaly_score=sculpts.ZeroBias_anomaly_score, dataset=sculpts.ZeroBias_data, area='top', percentage=0.05)
+        top_NG_pre = sculpt_study.select_anomalous_datapoints(anomaly_score=sculpts.NuGun_anomaly_score, dataset=sculpts.NuGun_data, area='top', percentage=0.05)
+
+        self.top_NGT = top_NG_pre[(top_NG_pre[:, 63] > 30) & (top_NG_pre[:, 66] > 30)]
+        self.NGT_weights = self.get_weights()
+        self.top_ZBT = top_ZB_pre[(top_ZB_pre[:, 63] > 30) & (top_ZB_pre[:, 66] > 30)]
+
+
+    def get_weights(self):
+        weights_jet1 = self.dataset_weights(self.top_NGT[:,63], self.top_NGT[:,64], self.ratio1, jet=1)
+        weights_jet2 = self.dataset_weights(self.top_NGT[:,63+3], self.top_NGT[:,64+3], self.ratio1, jet=2)
+        return list(map(self.multiply_entries, weights_jet1, weights_jet2))
+
+    def multiply_entries(self, entry1, entry2):
+        return entry1*entry2
+
+    def get_grids(self, ZB_Bottom, NG_Bottom):
+        num_bins_x = 60
+        quantiles_x = np.linspace(0, 1, num_bins_x + 1)
+        bin_edge_x1 = np.quantile(ZB_Bottom[(ZB_Bottom[:, 63] > 30) & (ZB_Bottom[:, 66] > 30), 63], quantiles_x)
+
+        # jet 1
+        self.NGB_jet1 = plt.hist2d(NG_Bottom[(NG_Bottom[:, 63] > 30) & (NG_Bottom[:, 66] > 30), 63], 
+                NG_Bottom[(NG_Bottom[:, 63] > 30) & (NG_Bottom[:, 66] > 30), 64], 
+                bins=[bin_edge_x1,40], norm=mcolors.LogNorm(), cmap='rainbow')
+        plt.colorbar(label='Count')
+        plt.xlabel('jet 1 p_T')
+        plt.ylabel('jet 1 $\\eta$')
+        # plt.xlim(30,160)
+        plt.show()
+
+        # jet 2
+        self.NGB_jet2 =  plt.hist2d(NG_Bottom[(NG_Bottom[:, 63] > 30) & (NG_Bottom[:, 66] > 30), 63+3], 
+                NG_Bottom[(NG_Bottom[:, 63] > 30) & (NG_Bottom[:, 66] > 30), 64+3], 
+                bins=[bin_edge_x1,40], norm=mcolors.LogNorm(), cmap='rainbow')
+        plt.colorbar(label='Count')
+        plt.xlabel('jet 2 p_T')
+        plt.ylabel('jet 2 $\\eta$')
+        plt.show()
+
+
+        # jet 1
+        self.ZBB_jet1 = plt.hist2d(ZB_Bottom[(ZB_Bottom[:, 63] > 30) & (ZB_Bottom[:, 66] > 30), 63], 
+                ZB_Bottom[(ZB_Bottom[:, 63] > 30) & (ZB_Bottom[:, 66] > 30), 64], 
+                bins=[bin_edge_x1,40], norm=mcolors.LogNorm(), cmap='rainbow')
+        plt.colorbar(label='Count')
+        plt.xlabel('jet 1 p_T')
+        plt.ylabel('jet 1 $\\eta$')
+        plt.show()
+
+        # jet 2
+        self.ZBB_jet2 =  plt.hist2d(ZB_Bottom[(ZB_Bottom[:, 63] > 30) & (ZB_Bottom[:, 66] > 30), 63+3], 
+                ZB_Bottom[(ZB_Bottom[:, 63] > 30) & (ZB_Bottom[:, 66] > 30), 64+3], 
+                bins=[bin_edge_x1,40], norm=mcolors.LogNorm(), cmap='rainbow')
+        plt.colorbar(label='Count')
+        plt.xlabel('jet 2 p_T')
+        plt.ylabel('jet 2 $\\eta$')
+        plt.show()
+
+        NGB_jet1_new = self.impute_values(self.NGB_jet1)
+        NGB_jet2_new = self.impute_values(self.NGB_jet2)
+        ZBB_jet1_new = self.impute_values(self.ZBB_jet1)
+        ZBB_jet2_new = self.impute_values(self.ZBB_jet2)
+
+        ratio1 = ZBB_jet1_new/NGB_jet1_new
+        ratio1[np.isnan(ratio1)] = 0
+        ratio1[np.isinf(ratio1)] = 1
+
+        ratio2 = ZBB_jet2_new/NGB_jet2_new
+        ratio2[np.isnan(ratio2)] = 0
+        ratio2[np.isinf(ratio2)] = 1
+
+        self.ratio1 = ratio1
+        self.ratio2 = ratio2
+
+
+
+    def search_right_value(self, datapoint_indices_pT, datapoint_indices_eta, grid, max_value=59):
+        counter = datapoint_indices_pT+1
+        print(f"counter before while: {counter}")
+        while grid[counter, datapoint_indices_eta] == 0:
+            print(grid[counter, datapoint_indices_eta] == 0)
+            print(f"counter within while {counter}")
+            if counter == max_value:
+                return max_value
+            else:
+                # print(f"count {counter}")
+                counter += 1
+        return counter
+
+
+    def linear_impute(self, datapoint_indices, grid):
+        datapoint_indices_pT = datapoint_indices[0]
+        datapoint_indices_eta = datapoint_indices[1]
+        left_value = grid[datapoint_indices_pT-1, datapoint_indices_eta]
+        print(f"pt aentry {datapoint_indices_pT} eta entry {datapoint_indices_eta} in linear impute before search_right_value")
+        right_value_index = self.search_right_value(datapoint_indices_pT, datapoint_indices_eta, grid)
+        right_value = grid[right_value_index, datapoint_indices_eta]
+        impute_value = ((left_value+right_value))/2
+        to_impute = list((datapoint_indices_pT, right_value_index-1))
+        return impute_value, to_impute
+
+
+    def impute_values(self, histo):
+        grid_1 = histo[0]
+        for p_T_index in range(1,len(histo[1])-2):
+            for eta_index in range(1,(len(histo[2]))-1):
+                if grid_1[p_T_index, eta_index] == 0:
+                    print(f"pt and eta entry is zero: {p_T_index} {eta_index}")
+                    impute_value, to_impute = self.linear_impute((p_T_index, eta_index), grid_1)
+                    print(f"impute value {impute_value}")
+                    for ind in to_impute:
+                        print(ind)
+                        grid_1[ind, eta_index] = impute_value
+                        print(f"imputation {grid_1[ind, eta_index]}, {impute_value}")
+        return grid_1
+
+
+    def find_bin2(self, jet_pt, jet_eta, all_grid):
+        jet_range = self.ZBB_jet2[1]
+        eta_range = self.ZBB_jet2[2]
+
+        pT_ind = np.digitize(jet_pt,jet_range[:-1])
+        eta_ind = np.digitize(jet_eta,eta_range[:-1])
+        calibration = all_grid[pT_ind-1, eta_ind-1]
+        return calibration
+
+    def find_bin1(self, jet_pt, jet_eta, all_grid):
+        jet_range = self.ZBB_jet1[1]
+        eta_range = self.ZBB_jet1[2]    
+
+        pT_ind = np.digitize(jet_pt,jet_range[:-1])
+        eta_ind = np.digitize(jet_eta,eta_range[:-1])
+        calibration = all_grid[pT_ind-1, eta_ind-1]
+        return calibration
+
+    def dataset_weights(self, jet_pts, jet_etas, all_grid, jet):
+        if jet == 2:
+            # return list(map(find_bin2, jet_pts, jet_etas, all_grid))
+            return list(self.find_bin2(jet_pts, jet_etas, all_grid))
+        else:
+            return list(self.find_bin1(jet_pts, jet_etas, all_grid))
+            # return list(map(find_bin1, jet_pts, jet_etas, all_grid))
